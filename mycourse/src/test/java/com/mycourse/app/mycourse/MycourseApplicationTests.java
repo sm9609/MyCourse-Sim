7818aaa18ff622071fb7955c8bbf2a4340d3e5c9
sm9609 <sm9609@rit.edu>
2025-11-18 13:11:16 -0500

    Initalizer

---
+++ mycourse/src/test/java/com/mycourse/app/mycourse/MycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycourse.app.mycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
