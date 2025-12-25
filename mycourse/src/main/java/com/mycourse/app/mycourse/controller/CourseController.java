b8eb93cf8b984be1255e7149f0c6918fac3d236c
sm9609 <sm9609@rit.edu>
2025-12-24 19:58:06 -0500

    Implement createCourse method in CourseController with error handling

--- mycourse/src/main/java/com/mycourse/app/mycourse/controller/CourseController.java
+++ mycourse/src/main/java/com/mycourse/app/mycourse/controller/CourseController.java
@@ -1,17 +1,50 @@
 package com.mycourse.app.mycourse.controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycourse.app.mycourse.model.Course;
+import com.mycourse.app.mycourse.persistence.CourseDAO;
 
 
 /**
  * Handles REST API calls for course recourses
- * 
+ * @author Samuel Mensah
  */
 @RestController
-@RequestMapping("courses")
+@RequestMapping("/courses")
 public class CourseController {
-    
-    public CourseController(){
-        
+    private static final Logger LOG = Logger.getLogger(CourseController.class.getName());
+    private final CourseDAO courseDAO;
+
+
+    public CourseController(CourseDAO courseDAO){
+        this.courseDAO = courseDAO;
+    }
+
+    @PostMapping(
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Course> createCourse(@RequestBody Course course){
+        LOG.log(Level.INFO, "POST /courses {0}", course);
+        try{
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-Type", "application/json");
+            return new ResponseEntity<>(courseDAO.createCourse(course),header,HttpStatus.OK);
+        }
+        catch(IOException e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
